14919193a51ce6b545b164a1ec4f55e27f063653
ArmanAnsari <armanansari62010@gmail.com>
2025-01-12 23:01:09 +0000

    to add the file in the github

---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,5 @@
+<div>
+    {{-- Because she competes with no one, no one can compete with her. --}}
+
+    <h1>Hello this is Counter page</h1>
+</div>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   
+    <title>LiveWire blog</title>
+    @livewireStyles
+</head>
+<body>
+
+  @livewire('counter')
+    
+@livewireScripts
+</body>
+</html>
